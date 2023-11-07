585c6f1dc99e6793fc154388036b2928dd6c2a94
Matthias Hryniszak <padcom@gmail.com>
2023-11-07 21:19:43 +0100

    Added BNE instruction

--- lib/ops/BNE.test.ts
+++ lib/ops/BNE.test.ts
@@ -3,15 +3,15 @@
 /* eslint-disable max-lines-per-function */
 /* eslint-disable key-spacing */
 import { describe, it, expect } from 'vitest'
-import { BEQ } from './BEQ'
+import { BNE } from './BNE'
 import { Registers } from '../infrastructure/Registers'
 import { uint32, uint5 } from '../infrastructure/Memory'
 
-describe('BEQ - load upper immediate', () => {
-  const op = new BEQ()
+describe('BNE - load upper immediate', () => {
+  const op = new BNE()
 
   it('will recognize', () => {
-    expect(op.recognize(0b0_000000_00000_00000_000_0000_0_1100011)).toBe(true)
+    expect(op.recognize(0b0_000000_00000_00000_001_0000_0_1100011)).toBe(true)
   })
 
   interface TestCase {
@@ -25,11 +25,11 @@ describe('BEQ - load upper immediate', () => {
   }
 
   const cases: TestCase[] = [{
-    instruction: 0b0_000000_11111_00101_000_0100_0_1100011,
+    instruction: 0b0_000000_11111_00101_001_0100_0_1100011,
     rs1        : Registers.t0,
     rs1I       : 0x1234,
     rs2        : Registers.t6,
-    rs2I       : 0x1234,
+    rs2I       : 0x1235,
     pcI        : 0x10000,
     pcO        : 0x10008,
   }, {
@@ -39,7 +39,7 @@ describe('BEQ - load upper immediate', () => {
     rs2        : Registers.t6,
     rs2I       : 0x1234,
     pcI        : 0x10000,
-    pcO        : 0x10008,
+    pcO        : 0x10000,
   }]
 
   cases.forEach(({ instruction, rs1, rs1I, rs2, rs2I, pcI, pcO }) => {
